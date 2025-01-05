7d747692a95d6283a3235fe22bbde78d244b9ef3
Alfin Pratama Efendy <alfin1993@gmail.com>
2025-01-05 19:23:34 +0700

    add domain for app config

--- config/model/app.go
+++ config/model/app.go
@@ -1,6 +1,7 @@
 package model
 
 type app struct {
-	Name string `mapstructure:"name"`
-	Mode string `mapstructure:"mode"`
+	Name   string `mapstructure:"name"`
+	Mode   string `mapstructure:"mode"`
+	Domain string `mapstructure:"domain"`
 }
