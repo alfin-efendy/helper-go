bde5e885353db73049f8fa8957699ef48f7804f9
Alfin Pratama Efendy <alfin1993@gmail.com>
2025-01-09 14:17:16 +0700

    add function SetRawResponse

--- server/restapi/response.go
+++ server/restapi/response.go
@@ -11,3 +11,9 @@ func SetData(ctx *gin.Context, data interface{}) {
 func SetPaggination(ctx *gin.Context, page PageResponse) {
 	ctx.Set(pageStr, page)
 }
+
+func SetRawResponse(ctx *gin.Context, httpCode int, message string) {
+	ctx.JSON(httpCode, gin.H{
+		"message": message,
+	})
+}
